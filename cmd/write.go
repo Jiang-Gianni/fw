eaac14bed0a3108b75f42cd2bbd701b08bb41dd2
gianni <jiang.gianni297@gmail.com>
2025-09-17 13:06:40 +0200

    refactor

--- cmd/write.go
+++ cmd/write.go
@@ -1,12 +1,16 @@
 package cmd
 
 import (
+	_ "embed"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/spf13/viper"
 )
+
+//go:embed fw/fw.go
+var fwGo []byte
 
 func Generate() {
 	dir := viper.GetString("dir")
@@ -16,9 +20,16 @@ func Generate() {
 		log.Fatal(err)
 	}
 	outputFile := dir + "/" + filename
-	os.WriteFile(outputFile, []byte(FwTemplate()), 0777)
+	f, err := os.Create(outputFile)
+	if err != nil {
+		log.Panicf("os.Create[%T]: %v", err, err)
+	}
+	defer f.Close()
+	_, err = f.Write(fwGo)
+	if err != nil {
+		log.Panicf("f.Write[%T]: %v", err, err)
+	}
 	fmt.Println("File Created : ", outputFile)
-	fmt.Println("Start Watching by running:")
+	fmt.Println("Start by running:")
 	fmt.Println("go run ", outputFile)
-	fmt.Println()
 }
